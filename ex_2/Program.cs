d9594a6b5f0538513b6181d62a36a230607d13ed
Lopushka <sv13yat@gmail.com>
2022-11-30 14:27:50 +0300

    add EX_2_Max_3_numder

---
+++ ex_2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Give give me a number_A: ");
+int numderA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Give give me a number_B: ");
+int numderB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Give give me a number_С: ");
+int numderС = Convert.ToInt32(Console.ReadLine());
+int MaxNamder = numderA;
+
+if (numderA > MaxNamder) MaxNamder = numderA;
+if (numderB > MaxNamder) MaxNamder = numderB;
+if (numderС > MaxNamder) MaxNamder = numderС;
+
+Console.WriteLine($"Max:{MaxNamder}");
