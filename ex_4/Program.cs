38d128e5d9a25d4ebcbda1dbd8d7a7cb6fe05082
Lopushka <sv13yat@gmail.com>
2022-11-30 15:43:16 +0300

    add EX_04_Even_numders.

---
+++ ex_4/Program.cs
@@ -0,0 +1,35 @@
+﻿// Без вывода "0".
+Console.Write("Give give me a number_A: ");
+int numderA = Convert.ToInt32(Console.ReadLine());
+
+
+for (int even = 1; even <= numderA; even++)
+{
+    if (even % 2 == 0)
+    {
+        Console.WriteLine($"even numbers: {even} ");
+    }
+}
+
+
+
+// если нужено вывести "0"
+// Console.Write("Give give me a number_A: ");
+
+// int numderA = Convert.ToInt32(Console.ReadLine());
+
+
+// for (int even = 0; even <= numderA; even++)
+// {
+//     if (even % 2 == 0)
+//     {
+//         Console.WriteLine($"even numbers: {even} ");
+//     }
+// }
+
+
+
+
+
+
+   
