cfba57c9537678177593d3c9f25205f5ce879227
Lopushka <sv13yat@gmail.com>
2022-11-30 14:36:00 +0300

    add EX_3_Even\odd

---
+++ ex_3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Give give me a number_A: ");
+int numderA = Convert.ToInt32(Console.ReadLine());
+
+if (numderA % 2 == 0)
+{
+    Console.WriteLine($"number {numderA} is even");
+}
+else
+{
+    Console.WriteLine($"number {numderA} is odd");
+}
